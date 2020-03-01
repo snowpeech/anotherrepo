19e5657bb8432d234fb095c896da84efe78ee474
Lydia <snowpeech@gmail.com>
2020-02-29 20:44:37 -0600

    updated email address and removed netlify from form

--- web/php/contactform-process.php
+++ web/php/contactform-process.php
@@ -25,7 +25,7 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
+$EmailTo = "snowpeech@gmail.com";
 $Subject = "New message from Leno landing page";
 
 // prepare email body text
--- web/php/privacyform-process.php
+++ web/php/privacyform-process.php
@@ -25,7 +25,7 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
+$EmailTo = "snowpeech@gmail.com";
 $Subject = "New privacy request from Leno landing page";
 
 // prepare email body text
